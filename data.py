663e26fd92220ee90a1f193d160c0fcc6eba8fc1
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-02-06 13:58:22 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,23 @@
-number = input("Give me a number")
-if number == "":
-    print("even")
+""" x = int(input('give me a number'))
+if(x%2) == 0:
+    print('even')
 else:
-    print("odd")
+    print('odd') """
+#---------------------------------------------------------
+
+""" x = int(input('subtotal'))
+service = input('How was the service')
+if service == ("bad"):
+    print(int(x))
+elif service == ("okay"):
+    print(int(x*1.15))
+elif service == ("good"):
+    print(int(x*1.2))
+elif service == ('great'):
+    print(int(x*1.25)) """
+
+#------------------------------------------------------------------
+
+def factors(number):
+    factorslist = []
+    for i in irange
