1bf065ee0f11127b30025a90d3071b304b3c4591
Matthewb521 <158211659+Matthewb521@users.noreply.github.com>
2024-02-07 14:00:20 -0500

    Update data.py

--- data.py
+++ data.py
@@ -18,6 +18,30 @@ elif service == ('great'):
 
 #------------------------------------------------------------------
 
-def factors(number):
-    factorslist = []
-    for i in irange
+""" def get_all_factors(n):
+    factors = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            factors.append(i)
+    return factors 
+1
+number = int(input("Please enter a number: "))
+list_of_factors = get_all_factors(number)
+print("factors of {} are: {}".format(number,list_of_factors)) """
+
+#--------------------------------------------------------------------------------------
+
+def gcf(numX, numY):
+    if numX > numY:
+        x = numY
+    else:
+        x = numX
+    cf = []
+    for i in range(1,x+1):
+        if(numX,numY)%i == 0:
+            cf.append(i)
+    return cf
+    
+numX = int(input('please enter the first number:'))
+numY = int(input('please enter the second number:'))
+print(max(gcf))
